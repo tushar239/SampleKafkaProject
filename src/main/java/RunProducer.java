f6e380ce36d63b07fc307f9f8f6fb62e9b1b0966
Tushar Chokshi <tushar239@gmail.com>
2017-01-07 16:29:53 -0800

    some minor changes

--- src/main/java/RunProducer.java
+++ src/main/java/RunProducer.java
@@ -1,10 +1,11 @@
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author Tushar Chokshi @ 12/31/16.
  */
 public class RunProducer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         SimpleProducer.main(new  String[]{"testmultitopic"});
 
     }
